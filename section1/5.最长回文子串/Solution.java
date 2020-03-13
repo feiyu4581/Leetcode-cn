3d2efb012b2bae7106d8853e1e39ef19f4729683
zhuzx <zhuzx@ersoft.cn>
2020-03-13 17:45:01 +0800

    feat:添加最长回文子串的初始化代码

---
+++ section1/5.最长回文子串/Solution.java
@@ -0,0 +1,25 @@
+/*
+ * @lc app=leetcode.cn id=5 lang=java
+ *
+ * [5] 最长回文子串
+ */
+
+// @lc code=start
+class Solution {
+    public String longestPalindrome(String s) {
+        return "OK";
+    }
+
+    public static void main(String[] args) {
+        Solution solution = new Solution();
+        System.out.println(
+            solution.longestPalindrome("babad")
+        );
+
+        System.out.println(
+            solution.longestPalindrome("cbbd")
+        );
+    }
+}
+// @lc code=end
+
