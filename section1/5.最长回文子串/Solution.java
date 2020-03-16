48d74d3351701431f8b2bdf9973a8992cd19cf31
zhuzx <zhuzx@ersoft.cn>
2020-03-16 10:57:51 +0800

    feat:添加最长回文子串

--- section1/5.最长回文子串/Solution.java
+++ section1/5.最长回文子串/Solution.java
@@ -7,7 +7,46 @@
 // @lc code=start
 class Solution {
     public String longestPalindrome(String s) {
-        return "OK";
+        int maxLength = -1;
+        String maxString = "";
+        for (int i = 0; i < s.length(); i++)  {
+            int subMaxLength = getSubMaxLength(s, i, i);
+            if (maxLength < subMaxLength) {
+                maxLength = subMaxLength;
+                maxString = s.substring(i - subMaxLength, i + subMaxLength + 1);
+            }
+
+            if (i + 1 < s.length() && s.charAt(i) == s.charAt(i + 1)) {
+                subMaxLength = getSubMaxLength(s, i, i + 1);
+                if (maxLength <= subMaxLength && i + subMaxLength + 2 <= s.length()) {
+                    maxLength = subMaxLength;
+                    maxString = s.substring(i - subMaxLength, i + subMaxLength + 2);
+                }
+            }
+        }
+
+        return maxString;
+    }
+
+    private int getSubMaxLength(String s, int begain, int end) {
+        boolean isPalidrome = true;
+        int maxLength = 0;
+        for (int i = 0; i < s.length(); i++) {
+            if (begain - i >= 0 && end + i < s.length()) {
+                isPalidrome = s.charAt(begain - i) == s.charAt(end + i);
+                if (isPalidrome && i > maxLength) {
+                    maxLength = i;
+                }
+            } else {
+                break;
+            }
+
+            if (!isPalidrome) {
+                break;
+            }
+        }
+
+        return maxLength;
     }
 
     public static void main(String[] args) {
@@ -18,6 +57,18 @@ class Solution {
 
         System.out.println(
             solution.longestPalindrome("cbbd")
+        );
+
+        System.out.println(
+            solution.longestPalindrome("ccc")
+        );
+
+        System.out.println(
+            solution.longestPalindrome("abcda")
+        );
+
+        System.out.println(
+            solution.longestPalindrome("tattarrattat")
         );
     }
 }
