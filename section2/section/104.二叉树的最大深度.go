33adc93ac406ba29bc43cb43f725d968d93c5e69
zhuzx <zhengxiang.zhu@airudder.com>
2022-07-01 16:39:27 +0800

    feat(104)

---
+++ section2/section/104.二叉树的最大深度.go
@@ -0,0 +1,44 @@
+package section
+
+import "fmt"
+
+/*
+ * @lc app=leetcode.cn id=104 lang=golang
+ *
+ * [104] 二叉树的最大深度
+ */
+
+// @lc code=start
+/**
+ * Definition for a binary tree node.
+ * type TreeNode struct {
+ *     Val int
+ *     Left *TreeNode
+ *     Right *TreeNode
+ * }
+ */
+
+func recursionMaxDepth(root *TreeNode) int {
+	if root == nil {
+		return 0
+	}
+
+	leftDepth := recursionMaxDepth(root.Left)
+	rightDepth := recursionMaxDepth(root.Right)
+
+	if leftDepth > rightDepth {
+		return leftDepth + 1
+	}
+
+	return rightDepth + 1
+}
+
+func maxDepth(root *TreeNode) int {
+	return recursionMaxDepth(root)
+}
+
+func Test104() {
+	fmt.Println(maxDepth(ToNode([]int{3, 9, 20, 0, 0, 15, 7})) == 3)
+}
+
+// @lc code=end
