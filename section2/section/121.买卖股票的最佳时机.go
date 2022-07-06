17dea0b8af06055d327077548f7b4f732815e2a1
zhuzx <zhengxiang.zhu@airudder.com>
2022-07-06 17:41:44 +0800

    feat(121)

---
+++ section2/section/121.买卖股票的最佳时机.go
@@ -0,0 +1,38 @@
+package section
+
+import "fmt"
+
+/*
+ * @lc app=leetcode.cn id=121 lang=golang
+ *
+ * [121] 买卖股票的最佳时机
+ */
+
+// @lc code=start
+func maxProfit(prices []int) int {
+	if len(prices) == 0 {
+		return 0
+	}
+
+	profit := 0
+	start := prices[0]
+	for _, price := range prices {
+		if price > start {
+			if profit < (price - start) {
+				profit = price - start
+			}
+		} else {
+			start = price
+		}
+
+	}
+
+	return profit
+}
+
+func Test121() {
+	fmt.Println(maxProfit([]int{7, 1, 5, 3, 6, 4}) == 5)
+	fmt.Println(maxProfit([]int{7, 6, 4, 3, 1}) == 0)
+}
+
+// @lc code=end
