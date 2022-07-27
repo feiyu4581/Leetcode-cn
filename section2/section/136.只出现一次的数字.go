78eb8ceac131486ec5438ec637b3ef2ca78ef8c3
zhuzx <zhengxiang.zhu@airudder.com>
2022-07-27 20:10:54 +0800

    feat(136)

---
+++ section2/section/136.只出现一次的数字.go
@@ -0,0 +1,26 @@
+package section
+
+import "fmt"
+
+/*
+ * @lc app=leetcode.cn id=136 lang=golang
+ *
+ * [136] 只出现一次的数字
+ */
+
+// @lc code=start
+func singleNumber(nums []int) int {
+	result := 0
+	for _, num := range nums {
+		result = result ^ num
+	}
+
+	return result
+}
+
+func Test136() {
+	fmt.Println(singleNumber([]int{2, 2, 1}) == 1)
+	fmt.Println(singleNumber([]int{4, 1, 2, 1, 2}) == 4)
+}
+
+// @lc code=end
